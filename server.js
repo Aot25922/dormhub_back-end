f7e2c2157d0be3becff4e97865e20ec5308c06ba
Aot25922 <aottanapat25922@gmail.com>
2022-03-30 12:01:16 +0700

    add DB template connection & .env

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+require('dotenv').config()
 const express = require('express')
 const chalk = require('chalk')
 const debug = require('debug')('app')
+// const mysql = require('mysql')
 const app = express()
-const port = 3000
+const port = process.env.PORT
+
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'dbuser',
+//     password: 's3kreee7',
+//     database: 'my_db'
+//   })
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
