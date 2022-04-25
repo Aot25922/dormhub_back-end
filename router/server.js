fc615887be9149f0ec90d3788a426b78555d50da
thanasapL <thanasap.lee@outlook.com>
2022-04-25 17:17:31 +0700

    Edit home page

--- router/server.js
+++ router/server.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use('/dorm',dorms)
 
 app.get('/', async (req, res) => {
-  res.send('คนที่เปิดหน้านี้เสร่อชิบหาย')
+  res.send('Fuck You Developer Who Create This Project! (Aot)')
 })
 app.listen(port, async () => {
   debug(` listening on port : ${chalk.red(port)}`)
