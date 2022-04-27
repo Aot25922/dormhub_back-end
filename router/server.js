58b861655e19536095fe3636e1a3700d3538fdf8
nithach2321 <Nithach@outlook.co.th>
2022-04-27 14:27:41 +0700

    Change Port

--- router/server.js
+++ router/server.js
@@ -4,13 +4,13 @@ const chalk = require('chalk')
 const debug = require('debug')('app')
 const app = express()
 const cors = require('cors')
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3001
 const dorms = require('./dorm')
 app.use(cors())
 app.use('/dorm',dorms)
 
 app.get('/', async (req, res) => {
-  res.send('Hello World!!')
+  res.send('Hello World!')
 })
 app.listen(port, async () => {
   debug(` listening on port : ${chalk.red(port)}`)
