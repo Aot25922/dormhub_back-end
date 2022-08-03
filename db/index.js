3485e2f143a0d9f1f1c2377d7c2dd30e7fadd575
nithach2321 <Nithach@outlook.co.th>
2022-08-03 12:01:23 +0700

    [Update] Change MySQL Port

--- db/index.js
+++ db/index.js
@@ -12,7 +12,7 @@ const sequelize = new Sequelize(
   'int365',
   '9Q7@e4>+#p;+LHpYd_GU2Y$.a?\\uhCg*', {
     host: process.env.Database_URL || 'mysql.dormhub.works',
-    port: process.env.Database_Port || '3306',
+    port: process.env.Database_Port || '33306',
     dialect: 'mysql',
     define: {
       timestamps: false
